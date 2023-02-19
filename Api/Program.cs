5fb014b4952571d7dc15a7dccd363510e15f885e
Manuel Rojas <manuelrojasb57@gmail.com>
2023-02-19 01:29:53 -0500

    Se agrega cadena de conexión a base de datos y se referencian las interfaces del Patrón Repositorio.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,12 +1,28 @@
+using Data.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repository;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+//Se agrega la conexión a base de datos SQL
+builder.Services.AddDbContext<ApiRestDbManuelRojasContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//Se agregan la referencias del patron Repository
+builder.Services.AddScoped<IAccountRepository, ServiceAccountRepository>();
+builder.Services.AddScoped<IClientRepository, ServiceClientRepository>();
+builder.Services.AddScoped<IMovementRepository, ServiceMovementRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
