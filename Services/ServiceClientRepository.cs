668b083f40585cef01b85cfb16079aad16d4d24a
Manuel Rojas <manuelrojasb57@gmail.com>
2023-02-19 01:37:34 -0500

    Se agregan las clases que implementan el patrón Repository.

---
+++ Services/ServiceClientRepository.cs
@@ -0,0 +1,34 @@
+﻿using Data.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace Services
+{
+    public class ServiceClientRepository : IClientRepository
+    {
+        public Task CreateClientAsync(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteClientAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Cliente>> GetAllClientsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cliente> GetClientByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateClientAsync(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ServiceMovementRepository.cs
@@ -0,0 +1,38 @@
+﻿using Data.EntityFramework.Entities;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ServiceMovementRepository : IMovementRepository
+    {
+        public Task CreateMovementAsync(Movimiento movimiento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovementAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movimiento>> GetAllMovementsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movimiento> GetMovementByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateMovementAsync(Movimiento movimiento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
