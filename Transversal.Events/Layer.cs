40178eef0f067c55a64c73c46999b3a748456ae9
Manuel Rojas <manuelrojasb57@gmail.com>
2023-02-19 02:00:16 -0500

    Se agrega proyecto para el manejo de eventos.

---
+++ Transversal.Events/Layer.cs
@@ -0,0 +1,9 @@
+﻿namespace Transversal.Events
+{
+    public enum Layer
+    {
+        Api = 1,
+        Business = 2,
+        Data = 3
+    }
+}
---
+++ Transversal.Events/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace Transversal.Events
+{
+    public class Notification
+    {
+        public Exception Exception { get; set; }
+
+        public Layer Layer { get; set; }
+
+        public Notification(Exception exception, Layer layer)
+        {
+            Exception = exception;
+            Layer = layer;
+        }
+    }
+}
