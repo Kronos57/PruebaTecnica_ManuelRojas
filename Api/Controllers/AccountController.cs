bbd0a0127a02e0edc3ea6c2c85d5464255b027da
Manuel Rojas <manuelrojasb57@gmail.com>
2023-02-19 01:25:20 -0500

    Se agregan controladores.

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Data.EntityFramework.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using Data.Clients;
+using Data.Accounts;
+using Services;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {    
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cuenta>>> GetCuentas()
+        {          
+            DataAccountGetList dataAccountGetList = new DataAccountGetList();
+
+            if (true)
+            {
+
+            }
+
+            //var cuentas = await _accountRepository.GetAllAccountsAsync();
+            //return Ok(cuentas);
+        }
+
+
+    }
+}
