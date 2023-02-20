7173683c23a1b576452a80a365bfe381ff022d18
Manuel Rojas <manuelrojasb57@gmail.com>
2023-02-20 11:01:50 -0500

    Se agrega proyecto de capa de negocio transversal.

---
+++ Business.Transversal/BusinessUtilsTransversal.cs
@@ -0,0 +1,31 @@
+﻿using Transversal.Entities;
+
+namespace Business.Transversal
+{
+    public class BusinessUtilsTransversal
+    {
+        public string GetTipoDeCuenta(int enumNumber)
+        {
+            string? nombreEnum = Enum.GetName(typeof(AccountTypeEnum), enumNumber);
+
+            if (string.IsNullOrEmpty(nombreEnum))
+            {
+                nombreEnum = "Sin Definir";
+            }
+
+            return nombreEnum;
+        }
+
+        public string GetEstado(bool estado)
+        {
+            string? nombreEstado = estado ? Enum.GetName(typeof(StateEnum), 1) : Enum.GetName(typeof(StateEnum), 0);
+
+            if (string.IsNullOrEmpty(nombreEstado))
+            {
+                nombreEstado = "Estado Desconocido";
+            }
+
+            return nombreEstado;
+        }
+    }
+}
