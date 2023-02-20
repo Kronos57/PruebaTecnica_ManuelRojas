855aff638607b5be936c897718c16ab69852f97a
Manuel Rojas <manuelrojasb57@gmail.com>
2023-02-20 11:00:44 -0500

    Se actualizan los controladores.

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -1,31 +1,87 @@
-﻿using Data.EntityFramework.Entities;
-using Microsoft.AspNetCore.Http;
+﻿using Data.Accounts;
 using Microsoft.AspNetCore.Mvc;
-using Repository;
-using Data.Clients;
-using Data.Accounts;
-using Services;
+using Transversal.Entities.DTO;
+using Transversal.Strategy;
 
 namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
-    {    
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Cuenta>>> GetCuentas()
-        {          
+    {
+        [HttpGet("GetAll")]
+        public ActionResult<IEnumerable<AccountSearchDTO>> GetAll()
+        {
             DataAccountGetList dataAccountGetList = new DataAccountGetList();
 
-            if (true)
+            if (dataAccountGetList.Execute() == StateStrategy.Success)
             {
-
+                return Ok(dataAccountGetList.Result);
             }
-
-            //var cuentas = await _accountRepository.GetAllAccountsAsync();
-            //return Ok(cuentas);
+            else
+            {
+                return BadRequest(dataAccountGetList.GetException());
+            }
         }
 
+        [HttpGet("GetById")]
+        public ActionResult<IEnumerable<AccountSearchDTO>> GetById(int id)
+        {
+            DataAccountGetById dataAccountGetById = new DataAccountGetById(id);
 
+            if (dataAccountGetById.Execute() == StateStrategy.Success)
+            {
+                return Ok(dataAccountGetById.Result);
+            }
+            else
+            {
+                return BadRequest(dataAccountGetById.GetException());
+            }
+        }
+
+        [HttpPost("Create")]
+        public ActionResult<IEnumerable<AccountSearchDTO>> Create(AccountDTO accountDTO)
+        {
+            DataAccountCreate dataAccountCreate = new DataAccountCreate(accountDTO);
+
+            if (dataAccountCreate.Execute() == StateStrategy.Success)
+            {
+                return Ok(dataAccountCreate.Result);
+            }
+            else
+            {
+                return BadRequest(dataAccountCreate.GetException());
+            }
+        }
+
+        [HttpPut("Update")]
+        public ActionResult<IEnumerable<AccountSearchDTO>> Update(AccountDTO accountDTO)
+        {
+            DataAccountUpdate dataAccountUpdate = new DataAccountUpdate(accountDTO);
+
+            if (dataAccountUpdate.Execute() == StateStrategy.Success)
+            {
+                return Ok(dataAccountUpdate.Result);
+            }
+            else
+            {
+                return BadRequest(dataAccountUpdate.GetException());
+            }
+        }
+
+        [HttpDelete("Delete")]
+        public ActionResult<IEnumerable<AccountSearchDTO>> Delete(int id)
+        {
+            DataAccountDelete dataAccountDelete = new DataAccountDelete(id);
+
+            if (dataAccountDelete.Execute() == StateStrategy.Success)
+            {
+                return Ok(dataAccountDelete.Result);
+            }
+            else
+            {
+                return BadRequest(dataAccountDelete.GetException());
+            }
+        }
     }
 }
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,24 +1,29 @@
 using Data.EntityFramework.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Repository;
-using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-//Se agrega la conexión a base de datos SQL
-builder.Services.AddDbContext<ApiRestDbManuelRojasContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+//Se intenta agregar la conexión a base de datos SQL desde el achivo appsettings.json pero no la toma,
+//por lo cual se deja la cadena de conexión en el método directamente OnConfiguring de la clase
+//ApiRestDbManuelRojasContext generada con el EntityFrameworkCore para el DbContext
 
-//Se agregan la referencias del patron Repository
-builder.Services.AddScoped<IAccountRepository, ServiceAccountRepository>();
-builder.Services.AddScoped<IClientRepository, ServiceClientRepository>();
-builder.Services.AddScoped<IMovementRepository, ServiceMovementRepository>();
+//var configuration = new ConfigurationBuilder()
+//    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//    .Build();
+
+//var conexionString = configuration.GetConnectionString("ApiDB");
+
+//builder.Services.AddDbContext<ApiRestDbManuelRojasContext>(options =>
+//    options.UseSqlServer(conexionString));
+
+//Se agregan la referencias del Patron Repository pero se omite ya que genera error con la capa de Data
+//builder.Services.AddScoped<IRepository<Cuenta>, AccountRepository>();
+//builder.Services.AddScoped<IRepository<Cliente>, ClientRepository>();
+//builder.Services.AddScoped<IRepository<Movimiento>, MovementRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
