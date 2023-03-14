a63a9d1e2b7d049e6407c9f90c588de22251ee82
Manuel Rojas <manuelrojasb57@gmail.com>
2023-03-14 06:44:05 -0500

    Se implementa el uso de transacciones en la capa de Negocio.

--- Business.Movements/BusinessMovementValidateCreate.cs
+++ Business.Movements/BusinessMovementValidateCreate.cs
@@ -1,5 +1,6 @@
 ﻿using Data.Accounts;
 using Data.Movements;
+using System.Transactions;
 using Transversal.Entities;
 using Transversal.Entities.DTO;
 using Transversal.Strategy;
@@ -10,6 +11,7 @@ namespace Business.Movements
     public class BusinessMovementValidateCreate : BusinessStrategy
     {
         private MovementDTO movementDTO;
+
         public BusinessMovementValidateCreate(MovementDTO movementDTO)
         {
             this.movementDTO = movementDTO;
@@ -17,81 +19,232 @@ namespace Business.Movements
 
         protected override void Process()
         {
-            if (movementDTO != null)
+            using (var scope = new TransactionScope())//Nueva transacción
             {
-                if (State == StateStrategy.Execution)
+                if (movementDTO != null)
                 {
-                    //Obtener la información de la Cuenta
-                    DataAccountGetById dataAccountGetById = new DataAccountGetById(movementDTO.IdCuenta);
-
-                    if (dataAccountGetById.Execute() == StateStrategy.Success)
+                    if (State == StateStrategy.Execution)
                     {
-                        AccountSearchDTO accountSearchDTO = (AccountSearchDTO)dataAccountGetById.Result;
-
-                        if (accountSearchDTO != null)
+                        if (ValidarCuenta(movementDTO.IdCuenta))
                         {
-                            DataMovementGetList dataMovementGetList = new DataMovementGetList();
-
-                            if (dataMovementGetList.Execute() == StateStrategy.Success)
+                            if (State == StateStrategy.Execution)
                             {
-                                int tipoMovimiento = movementDTO.IdTipoMovimiento;
-                                decimal saldoActual = 0;
-                                decimal valorMovimiento = movementDTO.Valor;
-                                decimal nuevoSaldoDisponible = 0;
+                                decimal saldoActual = ObtenerSaldoActual();
 
-                                List<MovementSearchDTO> movementsCliente = (List<MovementSearchDTO>)dataMovementGetList.Result;
-
-                                //Filtrar los movimienos de la Cuenta consultada
-                                movementsCliente = movementsCliente.FindAll(x => x.IdCuenta == movementDTO.IdCuenta);
-                                
-                                saldoActual = movementsCliente.OrderByDescending(x => x.IdMovimiento).First().SaldoDisponible;
-
-                                //Validar si el movimiento es Débito o Crédito
-                                //Retiro
-                                if (tipoMovimiento == Convert.ToInt32(MovementTypeEnum.Debito))
+                                //Se valida si el saldo de la cuenta es cero
+                                if (!ValidarSaldoCero(saldoActual))
                                 {
-                                    //Se valida si el saldo es suficiente
-                                    if (saldoActual >= valorMovimiento)
+                                    if (State == StateStrategy.Execution)
                                     {
-                                        //Se recalcula el Saldo Disponible 
-                                        nuevoSaldoDisponible = saldoActual - valorMovimiento;
-                                    }
-                                    else
-                                    {
-                                        SetException(VALIDATION_MESSAGES.SALDO_NO_DISPONIBLE);
-                                        return;
+                                        //Se valida el Cupo Diario de movimientos
+                                        if (!ValidarCupoDiario(movementDTO.IdCuenta))
+                                        {
+                                            if (State == StateStrategy.Execution)
+                                            {
+                                                //Se procesa el movimiento
+                                                ProcesarMovimiento(movementDTO.IdTipoMovimiento, movementDTO.Valor, saldoActual);
+                                            }
+                                        }
                                     }
                                 }
-                                //Consignación
-                                else if (tipoMovimiento == Convert.ToInt32(MovementTypeEnum.Credito))
-                                {
-                                    //Se recalcula el Saldo Disponible 
-                                    nuevoSaldoDisponible = saldoActual + valorMovimiento;
-                                }
-
-                                //Se asigna el nuevo valor del Saldo Disponible
-                                movementDTO.SaldoDisponible = nuevoSaldoDisponible;
-
-                                DataMovementCreate dataMovementCreate = new DataMovementCreate(movementDTO);
-
-                                //Se registra el nuevo Movimiento
-                                if (dataMovementCreate.Execute() == StateStrategy.Success)
-                                {
-                                    SetResponseResult(MOVEMENT_MESSAGES.MOVIMIENTO_CREADO);
-                                }
-                            }                            
-                        }
-                        else
-                        {
-                            SetException(EXCEPTION_MESSAGES.CUENTA_NO_EXISTE);
+                            }
                         }
                     }
                 }
+                else
+                {
+                    SetValidation(VALIDATION_MESSAGES.ERROR_MODELO_DATOS);
+                }
+
+                scope.Complete();
+            }
+        }
+
+        /// <summary>
+        /// Método que consulta si la cuenta existe y esta activa
+        /// </summary>
+        /// <returns></returns>
+        public bool ValidarCuenta(int idCuenta)
+        {
+            //Obtener la información de la Cuenta
+            bool cuentaActiva = false;
+            DataAccountGetById dataAccountGetById = new DataAccountGetById(idCuenta);
+
+            if (dataAccountGetById.Execute() == StateStrategy.Success)
+            {
+                AccountSearchDTO accountSearchDTO = (AccountSearchDTO)dataAccountGetById.Result;
+
+                if (accountSearchDTO != null)
+                {
+                    if (accountSearchDTO.NombreEstado.Equals(Enum.GetName(typeof(StateEnum), 1)))
+                    {
+                        cuentaActiva = true;
+                    }
+                    else
+                    {
+                        SetException(EXCEPTION_MESSAGES.CUENTA_INACTIVA);
+                    }
+                }
+                else
+                {
+                    SetException(EXCEPTION_MESSAGES.CUENTA_NO_EXISTE);
+                }
+            }
+
+            return cuentaActiva;
+        }
+
+        /// <summary>
+        /// Método que valida si el saldo de la cuenta esta en Cero
+        /// </summary>
+        /// <param name="saldoActual"></param>
+        /// <returns></returns>
+        private bool ValidarSaldoCero(decimal saldoActual)
+        {
+            bool saldoCero = false;
+
+            //Se valida Saldo en Cero
+            if (saldoActual <= 0)
+            {
+                saldoCero = true;
+                SetValidation(VALIDATION_MESSAGES.SALDO_CERO);
+            }
+
+            return saldoCero;
+        }
+
+        /// <summary>
+        /// Método que obtiene el saldo actual de la cuenta
+        /// </summary>
+        /// <returns></returns>
+        public decimal ObtenerSaldoActual()
+        {
+            decimal saldoActual = 0;
+            DataMovementGetList dataMovementGetList = new DataMovementGetList();
+
+            if (dataMovementGetList.Execute() == StateStrategy.Success)
+            {
+                List<MovementSearchDTO> movementsCliente = (List<MovementSearchDTO>)dataMovementGetList.Result;
+
+                //Filtrar los movimientos de la Cuenta consultada
+                movementsCliente = movementsCliente.FindAll(x => x.IdCuenta == movementDTO.IdCuenta);
+
+                saldoActual = movementsCliente.OrderByDescending(x => x.IdMovimiento).First().SaldoDisponible;
             }
             else
             {
-                SetValidation(VALIDATION_MESSAGES.ERROR_MODELO_DATOS);
+                SetException(EXCEPTION_MESSAGES.SALDO_NO_DISPONIBLE);
             }
+
+            return saldoActual;
+        }
+
+        /// <summary>
+        /// Método que procesa el movimiento y guarda el registro en base de datos
+        /// </summary>
+        /// <returns></returns>
+        public void ProcesarMovimiento(int tipoMovimiento, decimal valor, decimal saldoActual)
+        {
+            decimal nuevoSaldoDisponible = 0;
+
+            //Validar si el movimiento es Débito o Crédito
+            //Retiro
+            if (tipoMovimiento == Convert.ToInt32(MovementTypeEnum.Debito))
+            {
+                //Se valida si el saldo es suficiente
+                if (saldoActual >= valor)
+                {
+                    //Se recalcula el Saldo Disponible 
+                    nuevoSaldoDisponible = saldoActual - valor;
+                }
+                else
+                {
+                    SetException(VALIDATION_MESSAGES.SALDO_INSUFICIENTE);
+                }
+            }
+            //Consignación
+            else if (tipoMovimiento == Convert.ToInt32(MovementTypeEnum.Credito))
+            {
+                //Se recalcula el Saldo Disponible 
+                nuevoSaldoDisponible = saldoActual + valor;
+            }
+
+            //Se registra el Movimiento
+            RegistrarMovimiento(nuevoSaldoDisponible);
+        }
+
+        /// <summary>
+        /// Método que guarda el registro de un movimiento actualizando el saldo de la cuenta
+        /// </summary>
+        /// <param name="nuevoSaldo"></param>
+        private void RegistrarMovimiento(decimal nuevoSaldo)
+        {
+            if (State == StateStrategy.Execution)
+            {
+                //Se asigna el nuevo valor del Saldo Disponible
+                movementDTO.SaldoDisponible = nuevoSaldo;
+
+                DataMovementCreate dataMovementCreate = new DataMovementCreate(movementDTO);
+
+                //Se registra el nuevo Movimiento
+                if (dataMovementCreate.Execute() == StateStrategy.Success)
+                {
+                    SetResponseResult(MOVEMENT_MESSAGES.MOVIMIENTO_CREADO);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Método que retorna si se execede el cupo diario de movimientos
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <returns></returns>
+        public bool ValidarCupoDiario(int idCuenta)
+        {
+            bool excedeCupoDiario = false;
+            decimal cupoDiario = Convert.ToDecimal("700000");//Ejemplo Cupo Diario
+            decimal movimientosDia = ConsultarMovimientosDia(idCuenta);
+            decimal movimientoTotal = movimientosDia + movementDTO.Valor;
+
+            if (movimientoTotal > cupoDiario)
+            {
+                excedeCupoDiario = true;
+                SetValidation(VALIDATION_MESSAGES.CUPO_DIARIO_SUPERADO);
+            }
+
+            return excedeCupoDiario;
+        }
+
+        /// <summary>
+        /// Método que retorna el valor total de los movimientos realizados en una cuenta en el día actual
+        /// </summary>
+        /// <param name="idCuenta"></param>
+        /// <returns></returns>
+        public decimal ConsultarMovimientosDia(int idCuenta)
+        {
+            decimal valorMovimientosDia = 0;
+            DateTime fechaHoy = DateTime.Today;
+
+            DataMovementGetList dataMovementGetList = new DataMovementGetList();
+
+            if (dataMovementGetList.Execute() == StateStrategy.Success)
+            {
+                List<MovementSearchDTO> movementsCliente = (List<MovementSearchDTO>)dataMovementGetList.Result;
+
+                //Se consultan los movimientos realizados en la cuenta para el día actual
+                movementsCliente = movementsCliente.FindAll(x => x.IdCuenta == idCuenta);
+                movementsCliente = movementsCliente.Where(x => x.FechaMovimiento.Date == fechaHoy.Date).ToList();
+
+                if (movementsCliente.Any())
+                {
+                    foreach (var movement in movementsCliente)
+                    {
+                        valorMovimientosDia += movement.Valor;
+                    }
+                }
+            }
+
+            return valorMovimientosDia;
         }
     }
 }
--- Business.Reports/BusinessReportAccountStatus.cs
+++ Business.Reports/BusinessReportAccountStatus.cs
@@ -1,6 +1,7 @@
 ﻿using Data.Accounts;
 using Data.Clients;
 using Data.Movements;
+using System.Transactions;
 using Transversal.Entities.DTO;
 using Transversal.Entities.Reports.AccountStatus;
 using Transversal.Strategy;
@@ -19,108 +20,142 @@ namespace Business.Reports
 
         protected override void Process()
         {
-            if (accountStatusRequest != null)
+            using (var scope = new TransactionScope())//Nueva transacción
             {
-                if (accountStatusRequest.IdCliente == 0)
+                if (accountStatusRequest != null)
                 {
-                    SetValidation(VALIDATION_MESSAGES.ID_CLIENTE_OBLIGATORIO);
-                }
-
-                if (accountStatusRequest.FechaInicial == DateTime.MinValue)
-                {
-                    SetValidation(VALIDATION_MESSAGES.FECHA_INICIAL_OBLIGATORIA);
-                }
-
-                if (accountStatusRequest.FechaFinal == DateTime.MinValue)
-                {
-                    SetValidation(VALIDATION_MESSAGES.FECHA_FINAL_OBLIGATORIA);
-                }
-
-                if (accountStatusRequest.FechaInicial > accountStatusRequest.FechaFinal)
-                {
-                    SetValidation(VALIDATION_MESSAGES.ERROR_RANGO_FECHAS);
-                }
-
-                if (State == StateStrategy.Execution)
-                {
-                    //Obtener los datos del cliente
-                    DataClientGetById DataClientGetById = new DataClientGetById(accountStatusRequest.IdCliente);
-
-                    if (DataClientGetById.Execute() == StateStrategy.Success)
+                    if (ValidarCampos())
                     {
-                        ClientSearchDTO clientSearchDTO = (ClientSearchDTO)DataClientGetById.Result;
-
-                        if (clientSearchDTO != null)
+                        if (State == StateStrategy.Execution)
                         {
-                            DataAccountGetList dataAccountGetList = new DataAccountGetList();
+                            //Obtener los datos del cliente
+                            DataClientGetById DataClientGetById = new DataClientGetById(accountStatusRequest.IdCliente);
 
-                            if (dataAccountGetList.Execute() == StateStrategy.Success)
+                            if (DataClientGetById.Execute() == StateStrategy.Success)
                             {
-                                List<AccountSearchDTO> accounts = (List<AccountSearchDTO>)dataAccountGetList.Result;
+                                ClientSearchDTO clientSearchDTO = (ClientSearchDTO)DataClientGetById.Result;
 
-                                //Obtener las Cuentas del Cliente consultado
-                                accounts = accounts.FindAll(x => x.IdCliente == accountStatusRequest.IdCliente).ToList();
-
-                                if (accounts.Count > 0)
+                                if (clientSearchDTO != null)
                                 {
-                                    //Listado de Movimientos para el Reporte
-                                    List<AccountStatusResponse> movementsReport = new List<AccountStatusResponse>();
+                                    DataAccountGetList dataAccountGetList = new DataAccountGetList();
 
-                                    foreach (AccountSearchDTO account in accounts)
+                                    if (dataAccountGetList.Execute() == StateStrategy.Success)
                                     {
-                                        //Consultar los Movimientos asociados a cada cuenta del Cliente consultado
-                                        DataMovementGetList dataMovementGetList = new DataMovementGetList();
+                                        List<AccountSearchDTO> accounts = (List<AccountSearchDTO>)dataAccountGetList.Result;
 
-                                        if (dataMovementGetList.Execute() == StateStrategy.Success)
+                                        //Obtener las Cuentas del Cliente consultado
+                                        accounts = accounts.FindAll(x => x.IdCliente == accountStatusRequest.IdCliente).ToList();
+
+                                        if (accounts.Count > 0)
                                         {
-                                            List<MovementSearchDTO> movementsCliente = (List<MovementSearchDTO>)dataMovementGetList.Result;
-
-                                            //Se filtran los movimientos del CLiente consultado
-                                            movementsCliente = movementsCliente.FindAll(x => x.IdCuenta == account.IdCuenta);
-
-                                            //Se filtran por rango de fechas consultadas
-                                            movementsCliente = movementsCliente.Where(x => x.FechaMovimiento.Date >= accountStatusRequest.FechaInicial.Date
-                                            && x.FechaMovimiento.Date <= accountStatusRequest.FechaFinal.Date).ToList();
-
-                                            if (movementsCliente.Count > 0)
-                                            {
-                                                foreach (MovementSearchDTO movement in movementsCliente)
-                                                {
-                                                    movementsReport.Add(new AccountStatusResponse(movement.FechaMovimiento, clientSearchDTO.Nombre, account.NumeroCuenta,
-                                                        account.NombreTipoCuenta, account.SaldoInicial, movement.NombreEstado, movement.Valor,
-                                                        movement.SaldoDisponible));
-                                                }                                                
-                                            }
+                                            //Listado de Movimientos para el Reporte
+                                            GenerarReporte(accounts, clientSearchDTO);
                                         }
-                                    }
-
-                                    if (movementsReport.Count > 0)
-                                    {
-                                        movementsReport.OrderBy(x => x.Fecha).ThenBy(x =>x.NumeroCuenta);
-                                        SetResult(movementsReport);
-                                    }
-                                    else
-                                    {
-                                        SetException(VALIDATION_MESSAGES.CUENTA_SIN_MOVIMIENTOS);
+                                        else
+                                        {
+                                            SetException(VALIDATION_MESSAGES.CLIENTE_SIN_CUENTA_ASOCIADA);
+                                        }
                                     }
                                 }
                                 else
                                 {
-                                    SetException(VALIDATION_MESSAGES.CLIENTE_SIN_CUENTA_ASOCIADA);
+                                    SetException(EXCEPTION_MESSAGES.CLIENTE_NO_EXISTE);
                                 }
                             }
-                        }
-                        else
-                        {
-                            SetException(EXCEPTION_MESSAGES.CLIENTE_NO_EXISTE);
+
                         }
                     }
-
                 }
+                else
+                {
+                    SetValidation(VALIDATION_MESSAGES.ERROR_MODELO_DATOS);
+                }
+
+                scope.Complete();
+            }
+        }
+
+        /// <summary>
+        /// Método que valida que los campos obligatorios se hayan diligenciado
+        /// </summary>
+        /// <returns></returns>
+        private bool ValidarCampos() 
+        {
+            bool validacionOk = true;
+
+            if (accountStatusRequest.IdCliente == 0)
+            {
+                validacionOk = false;
+                SetValidation(VALIDATION_MESSAGES.ID_CLIENTE_OBLIGATORIO);
+            }
+
+            if (accountStatusRequest.FechaInicial == DateTime.MinValue)
+            {
+                validacionOk = false;
+                SetValidation(VALIDATION_MESSAGES.FECHA_INICIAL_OBLIGATORIA);
+            }
+
+            if (accountStatusRequest.FechaFinal == DateTime.MinValue)
+            {
+                validacionOk = false;
+                SetValidation(VALIDATION_MESSAGES.FECHA_FINAL_OBLIGATORIA);
+            }
+
+            if (accountStatusRequest.FechaInicial > accountStatusRequest.FechaFinal)
+            {
+                validacionOk = false;
+                SetValidation(VALIDATION_MESSAGES.ERROR_RANGO_FECHAS);
+            }
+
+            return validacionOk;
+        }
+
+        /// <summary>
+        /// Genera un reporte de los movimientos de cada cuenta de un cliente consultado
+        /// </summary>
+        /// <param name="accounts"></param>
+        /// <returns></returns>
+        private void GenerarReporte(List<AccountSearchDTO> accounts, ClientSearchDTO clientSearchDTO) 
+        {
+            //Listado de Movimientos para el Reporte
+            List<AccountStatusResponse> movementsReport = new List<AccountStatusResponse>();
+
+            foreach (AccountSearchDTO account in accounts)
+            {
+                //Consultar los Movimientos asociados a cada cuenta del Cliente consultado
+                DataMovementGetList dataMovementGetList = new DataMovementGetList();
+
+                if (dataMovementGetList.Execute() == StateStrategy.Success)
+                {
+                    List<MovementSearchDTO> movementsCliente = (List<MovementSearchDTO>)dataMovementGetList.Result;
+
+                    //Se filtran los movimientos del CLiente consultado
+                    movementsCliente = movementsCliente.FindAll(x => x.IdCuenta == account.IdCuenta);
+
+                    //Se filtran por rango de fechas consultadas
+                    movementsCliente = movementsCliente.Where(x => x.FechaMovimiento.Date >= accountStatusRequest.FechaInicial.Date
+                    && x.FechaMovimiento.Date <= accountStatusRequest.FechaFinal.Date).ToList();
+
+                    if (movementsCliente.Count > 0)
+                    {
+                        foreach (MovementSearchDTO movement in movementsCliente)
+                        {
+                            movementsReport.Add(new AccountStatusResponse(movement.FechaMovimiento, clientSearchDTO.Nombre, account.NumeroCuenta,
+                                account.NombreTipoCuenta, account.SaldoInicial, movement.NombreEstado, movement.Valor,
+                                movement.SaldoDisponible));
+                        }
+                    }
+                }
+            }
+
+            if (movementsReport.Count > 0)
+            {
+                movementsReport.OrderBy(x => x.Fecha).ThenBy(x => x.NumeroCuenta);
+                SetResult(movementsReport);
             }
             else
             {
-                SetValidation(VALIDATION_MESSAGES.ERROR_MODELO_DATOS);
+                SetException(VALIDATION_MESSAGES.CUENTA_SIN_MOVIMIENTOS);
             }
         }
     }
